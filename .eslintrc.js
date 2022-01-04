af1e639271bc111f26bcb6af208c54cd46ec4ee1
eman ebrahim <adla48770@gmail.com>
2022-01-04 17:47:26 +0200

    typography

--- .eslintrc.js
+++ .eslintrc.js
@@ -55,5 +55,6 @@ module.exports = {
     'no-nested-ternary': 0,
     '@typescript-eslint/ban-ts-comment': 0,
     'class-methods-use-this': 0,
+    'react/react-in-jsx-scope': 0,
   },
 };
---
+++ src/components/baseComponent/Typography/index.tsx
@@ -0,0 +1,14 @@
+import Wrapper from './style';
+import { IPropTypography } from './interface';
+
+const Typography: React.FC<IPropTypography> = ({
+  variant = 'p',
+  ...props
+}: IPropTypography) => {
+  return (
+    <Wrapper as={variant} {...props}>
+      {props.children}
+    </Wrapper>
+  );
+};
+export default Typography;
