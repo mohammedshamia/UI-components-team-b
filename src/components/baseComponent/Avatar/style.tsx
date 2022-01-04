1535e6c3c4f724c0a285524429d0c3f6d0a20a8c
ahmadysalah <a.ahmad.salah@gmail.com>
2022-01-04 18:42:31 +0200

    handle avatar props

--- src/components/baseComponent/Avatar/style.tsx
+++ src/components/baseComponent/Avatar/style.tsx
@@ -9,18 +9,21 @@ const getSize = (size: IImageProps['size']) => {
       return '48px';
     case 'large':
       return '64px';
+    case 'xlarge':
+      return '96px';
     default:
       return '48px';
   }
 };
 
-const Avatar = styled.image<IImageProps>`
+const Avatar = styled.img<IImageProps>`
   width: ${props => getSize(props.size)};
   height: ${props => getSize(props.size)};
   border-radius: 50%;
+  border: ${props => (props.border ? '1px solid #ccc' : 'none')};
   border-width: ${props => (props.border ? '1px' : '0')};
   box-shadow: ${props =>
-    props.shadow ? '0px 0px 2px rgba(0, 0, 0, 0.25)' : 'none'};
+    props.shadow ? '0px 2px 5px rgba(0, 0, 0, 0.25)' : 'none'};
 `;
 
 export default Avatar;
