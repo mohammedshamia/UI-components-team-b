2a6f9b0e42fcab09be8de97dbeba0ebae3fa9284
ahmadysalah <a.ahmad.salah@gmail.com>
2022-01-05 21:00:41 +0200

    update sider

---
+++ src/components/layoutComponent/Navbar/index.tsx
@@ -0,0 +1,12 @@
+import { NavbarComponent } from './style';
+// interface INavbar {}
+
+const Navbar: React.FC<any> = () => {
+  return (
+    <div>
+      <h1>Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/layoutComponent/Navbar/style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const NavbarComponent = styled.div`
+  /* background: red; */
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  grid-template-columns: repeat(5, minmax(0, 1fr));
+  gap: 0.5rem;
+`;
