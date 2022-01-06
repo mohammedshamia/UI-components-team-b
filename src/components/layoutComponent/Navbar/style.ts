59607a76489a1f67e650ae49505b430fccd8721e
ahmadysalah <a.ahmad.salah@gmail.com>
2022-01-06 17:19:46 +0200

    update style

--- src/components/layoutComponent/Navbar/style.ts
+++ src/components/layoutComponent/Navbar/style.ts
@@ -33,7 +33,7 @@ export const NavbarSearchInput = styled.input`
   border-radius: 10px;
   padding: 9px;
   background: #f3f6f9;
-  width: 89%;
+  width: 85%;
   margin-left: auto;
   font-size: 0.8rem;
   font-weight: thin;
@@ -47,7 +47,7 @@ export const NavbarSearchContainer = styled.div`
   border: 1px solid #e5e5e5;
   border-radius: 10px;
   background: #f3f6f9;
-  width: 18em;
+  width: 20em;
   margin-left: auto;
   font-size: 0.8rem;
   font-weight: thin;
@@ -59,6 +59,14 @@ export const NavbarSearchContainer = styled.div`
 
 export const NavbarSearchIcon = styled.i`
   color: #3e5060;
+  font-size: 0.7rem;
+  background: #ffffff;
+  padding: 7px 10px 7px 10px;
+  margin-right: 2px;
+  border-radius: 5px;
+  &:hover {
+    background: #f3f6f9;
+  }
 `;
 
 export const NavbarLinksIcon = styled.i`
